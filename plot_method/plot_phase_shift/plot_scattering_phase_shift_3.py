f09700aa795e182abae2f95fd26ba4a2f2128545
w01 <jiangw@ornl.gov>
2019-02-22 18:01:10 -0500

    new file:   NNLO450/set20_Nmax14/np_phase_shift_1.eps 	new file:   NNLO450/set20_Nmax14/np_phase_shift_2.eps 	new file:   NNLO450/set20_Nmax14/pp_phase_shift.eps 	new file:   plot_phase_shift/.plot_scattering_phase_shift_3.py.swp 	modified:   plot_phase_shift/np_phase_shift_1.eps 	modified:   plot_phase_shift/np_phase_shift_2.eps 	modified:   plot_phase_shift/np_phase_shift_new.eps 	modified:   plot_phase_shift/plot_scattering_phase_shift_2.py 	new file:   plot_phase_shift/plot_scattering_phase_shift_3.py 	modified:   plot_phase_shift/pp_phase_shift.eps 	new file:   plot_phase_shift/residual_data_394.txt 	renamed:    plot_phase_shift/residual_data.txt -> plot_phase_shift/residual_data_450.txt

--- plot_method/plot_phase_shift/plot_scattering_phase_shift_3.py
+++ plot_method/plot_phase_shift/plot_scattering_phase_shift_3.py
@@ -31,9 +31,9 @@ def input_residual_data(file_path,raw_data):
         print ('raw_data='+str(raw_data))
 
 
-def plot_phase_shift(file_path):
-    with open(file_path,'r') as f:
-        count = len(open(file_path,'rU').readlines())
+def plot_phase_shift(file_path1,file_path2):
+    with open(file_path1,'r') as f:
+        count = len(open(file_path1,'rU').readlines())
         data = f.readlines()
         loop2 = 0
         loop1 = 0
@@ -48,7 +48,10 @@ def plot_phase_shift(file_path):
     scattering_data_num = loop2
     print ('scattering_data_num='+str(scattering_data_num))
     raw_data_1 = np.zeros((scattering_data_num,4),dtype = np.float)
-    input_residual_data(file_path,raw_data_1)
+    raw_data_2 = np.zeros((scattering_data_num,4),dtype = np.float)
+    input_residual_data(file_path1,raw_data_1)
+    input_residual_data(file_path2,raw_data_2)
+
     loss = np.power(((raw_data_1[:,1] - raw_data_1[:,0])/raw_data_1[:,2]),2)
     tol_loss = np.sum(loss)/ len(loss)
     print ('loss='+str(tol_loss))
@@ -65,15 +68,21 @@ def plot_phase_shift(file_path):
     start_line = 30*data_points
     x_list     = raw_data_1[start_line:start_line+ data_points  ,3]  # energy
     y_list_1   = raw_data_1[start_line:start_line+data_points ,1]  # exp
-    y_list_2   = raw_data_1[start_line:start_line+data_points ,0]  # theo
+    y_list_2   = raw_data_1[start_line:start_line+data_points ,0]  # theo 450
+    y_list_3   = raw_data_2[start_line:start_line+data_points ,0]  # theo 394
     x_list_new = np.linspace(np.min(x_list),np.max(x_list),num=plot_interpol_count)
-    func       = interpolate.interp1d(x_list,y_list_2,kind=kind)
-    y_list_2_new = func(x_list_new) 
+    func_1       = interpolate.interp1d(x_list,y_list_2,kind=kind)
+    y_list_2_new = func_1(x_list_new) 
+    func_2       = interpolate.interp1d(x_list,y_list_3,kind=kind)
+    y_list_3_new = func_2(x_list_new) 
+ 
+
     #print('y_list='+str(y_list_2))
     #print('y_list_2_new='+str(y_list_2_new))
-    l_exp      = plt.scatter(x_list,y_list_1,color = 'k',s = point_size,marker ='.')
-    l_theo     = plt.plot   (x_list_new,y_list_2_new,color = 'b',linestyle = '-') 
-    
+    l_exp      = plt.plot(x_list,y_list_1,color = 'k',linestyle='',markersize=4, marker ='s',zorder=4,label='Granada PWA')
+    l_theo_450 = plt.plot   (x_list_new,y_list_2_new,color = 'g',linestyle = '--',zorder=3,label=r'$\Delta$NNLO$_{\rm{GO}}$(450)') 
+    l_theo_394 = plt.plot   (x_list_new,y_list_3_new,color = 'b',linestyle = '-.',zorder=2,label=r'$\Delta$NNLO$_{\rm{GO}}$(394)') 
+
     #plt.xlabel(fontsize = x_fontsize)
     plt.ylabel('$\delta(^3D_11$(deg)')
     plt.yticks(np.arange(-30,0,5),fontsize = y_fontsize)
@@ -81,6 +90,55 @@ def plot_phase_shift(file_path):
 
     plt.xlabel(r'$T_{\rm{Lab}}$(MeV)')
     #plt.legend(loc=2, bbox_to_anchor=(1.63,0.5),borderaxespad = 0.)
+
+
+    ## np 3S1
+    matplotlib.rcParams['xtick.direction'] = 'in' 
+    matplotlib.rcParams['ytick.direction'] = 'in' 
+    plt.subplot(223)
+    plt.tick_params(top=True,bottom=True,left=True,right=False)
+    start_line = 28*data_points
+    x_list     = raw_data_1[start_line:start_line+data_points,3]  # energy
+    y_list_1   = raw_data_1[start_line:start_line+data_points,1]  # exp
+    y_list_2   = raw_data_1[start_line:start_line+data_points,0]  # theo
+    y_list_3   = raw_data_2[start_line:start_line+data_points ,0]  # theo 394
+    func_1       = interpolate.interp1d(x_list,y_list_2,kind=kind)
+    y_list_2_new = func_1(x_list_new) 
+    func_2       = interpolate.interp1d(x_list,y_list_3,kind=kind)
+    y_list_3_new = func_2(x_list_new) 
+ 
+
+
+
+    for loop1 in range(len(y_list_1)):
+        if y_list_1[loop1] < 0:
+            y_list_1[loop1] = y_list_1[loop1]+180
+    for loop2 in range(len(y_list_2)):
+        if y_list_2[loop2] < 0:
+            y_list_2[loop2] = y_list_2[loop2]+180
+    for loop3 in range(len(y_list_3)):
+        if y_list_3[loop3] < 0:
+            y_list_3[loop3] = y_list_3[loop3]+180
+ 
+    x_list_new = np.linspace(np.min(x_list),np.max(x_list),num=plot_interpol_count)
+    func_1       = interpolate.interp1d(x_list,y_list_2,kind=kind)
+    y_list_2_new = func_1(x_list_new) 
+    func_2       = interpolate.interp1d(x_list,y_list_3,kind=kind)
+    y_list_3_new = func_2(x_list_new) 
+ 
+
+    l_exp      = plt.plot(x_list,y_list_1,color = 'k',linestyle='',markersize=4, marker ='s',zorder=4,label='Granada PWA')
+    l_theo_450 = plt.plot   (x_list_new,y_list_2_new,color = 'g',linestyle = '--',zorder=3,label=r'$\Delta$NNLO$_{\rm{GO}}$(450)') 
+    l_theo_394 = plt.plot   (x_list_new,y_list_3_new,color = 'b',linestyle = '-.',zorder=2,label=r'$\Delta$NNLO$_{\rm{GO}}$(394)') 
+
+
+
+    plt.ylabel('$\delta(^3S_1)$(deg)')
+    plt.yticks(np.arange(-50,201,50),fontsize = y_fontsize)
+    plt.xticks(np.arange(0,201,50),fontsize = y_fontsize)
+    plt.xlabel(r'$T_{\rm{Lab}}$(MeV)')
+
+
 
 
     ## np 1S0
@@ -92,45 +150,27 @@ def plot_phase_shift(file_path):
     x_list     = raw_data_1[start_line:start_line+data_points ,3]  # energy
     y_list_1   = raw_data_1[start_line:start_line+data_points,1]  # exp
     y_list_2   = raw_data_1[start_line:start_line+data_points,0]  # theo
+    y_list_3   = raw_data_2[start_line:start_line+data_points ,0]  # theo 394
     x_list_new = np.linspace(np.min(x_list),np.max(x_list),num=plot_interpol_count)
-    func       = interpolate.interp1d(x_list,y_list_2,kind=kind)
-    y_list_2_new = func(x_list_new) 
+    func_1       = interpolate.interp1d(x_list,y_list_2,kind=kind)
+    y_list_2_new = func_1(x_list_new) 
+    func_2       = interpolate.interp1d(x_list,y_list_3,kind=kind)
+    y_list_3_new = func_2(x_list_new) 
+ 
     #print('y_list='+str(y_list_2))
     #print('y_list_2_new='+str(y_list_2_new))
-    l_exp      = plt.scatter(x_list,y_list_1,color = 'k',s = point_size,marker ='.')
-    l_theo     = plt.plot   (x_list,y_list_2,color = 'b',linestyle = '-') 
+    l_exp      = plt.plot(x_list,y_list_1,color = 'k',linestyle='',markersize=4, marker ='s',zorder=4,label='Granada PWA')
+    l_theo_450 = plt.plot   (x_list_new,y_list_2_new,color = 'g',linestyle = '--',zorder=3,label=r'$\Delta$NNLO$_{\rm{GO}}$(450)') 
+    l_theo_394 = plt.plot   (x_list_new,y_list_3_new,color = 'b',linestyle = '-.',zorder=2,label=r'$\Delta$NNLO$_{\rm{GO}}$(394)') 
+
+
+
     
+    plt.legend(loc=4,bbox_to_anchor=(1,-0.2) ,fontsize=9)
     #plt.xlabel(fontsize = x_fontsize)
     plt.ylabel('$\delta(^1S_0)$(deg)')
     plt.yticks(np.arange(-40,81,20),fontsize = y_fontsize)
     plt.xticks(np.arange(0,201,50),[])
-
-    ## np 3S1
-    matplotlib.rcParams['xtick.direction'] = 'in' 
-    matplotlib.rcParams['ytick.direction'] = 'in' 
-    plt.subplot(223)
-    plt.tick_params(top=True,bottom=True,left=True,right=False)
-    start_line = 28*data_points
-    x_list     = raw_data_1[start_line:start_line+data_points,3]  # energy
-    y_list_1   = raw_data_1[start_line:start_line+data_points,1]  # exp
-    y_list_2   = raw_data_1[start_line:start_line+data_points,0]  # theo
-    for loop1 in range(len(y_list_1)):
-        if y_list_1[loop1] < 0:
-            y_list_1[loop1] = y_list_1[loop1]+180
-    for loop2 in range(len(y_list_2)):
-        if y_list_2[loop2] < 0:
-            y_list_2[loop2] = y_list_2[loop2]+180
-
-    x_list_new = np.linspace(np.min(x_list),np.max(x_list),num=plot_interpol_count)
-    func       = interpolate.interp1d(x_list,y_list_2,kind=kind)
-    y_list_2_new = func(x_list_new) 
-    l_exp      = plt.scatter(x_list,y_list_1,color = 'k',s = point_size,marker ='.')
-    l_theo     = plt.plot   (x_list_new,y_list_2_new,color = 'b',linestyle = '-') 
-
-    plt.ylabel('$\delta(^3S_1)$(deg)')
-    plt.yticks(np.arange(-50,201,50),fontsize = y_fontsize)
-    plt.xticks(np.arange(0,201,50),fontsize = y_fontsize)
-    plt.xlabel(r'$T_{\rm{Lab}}$(MeV)')
 
     ## np 3P0
     matplotlib.rcParams['xtick.direction'] = 'in' 
@@ -141,17 +181,24 @@ def plot_phase_shift(file_path):
     x_list     = raw_data_1[start_line:start_line+data_points,3]  # energy
     y_list_1   = raw_data_1[start_line:start_line+data_points,1]  # exp
     y_list_2   = raw_data_1[start_line:start_line+data_points,0]  # theo
+    y_list_3   = raw_data_2[start_line:start_line+data_points ,0]  # theo 394
     x_list_new = np.linspace(np.min(x_list),np.max(x_list),num=plot_interpol_count)
-    func       = interpolate.interp1d(x_list,y_list_2,kind=kind)
-    y_list_2_new = func(x_list_new) 
+    func_1       = interpolate.interp1d(x_list,y_list_2,kind=kind)
+    y_list_2_new = func_1(x_list_new) 
+    func_2       = interpolate.interp1d(x_list,y_list_3,kind=kind)
+    y_list_3_new = func_2(x_list_new) 
+ 
 
-    l_exp      = plt.scatter(x_list,y_list_1,color = 'k',s = point_size,marker ='.')
-    l_theo     = plt.plot   (x_list_new,y_list_2_new,color = 'b',linestyle = '-') 
+    l_exp      = plt.plot(x_list,y_list_1,color = 'k',linestyle='',markersize=4, marker ='s',zorder=4,label='Granada PWA')
+    l_theo_450 = plt.plot   (x_list_new,y_list_2_new,color = 'g',linestyle = '--',zorder=3,label=r'$\Delta$NNLO$_{\rm{GO}}$(450)') 
+    l_theo_394 = plt.plot   (x_list_new,y_list_3_new,color = 'b',linestyle = '-.',zorder=2,label=r'$\Delta$NNLO$_{\rm{GO}}$(394)') 
+
+
 
     plt.ylabel('$\delta(^3P_0)$(deg)')
     plt.yticks(np.arange(-60,21,10),fontsize = y_fontsize)
     plt.xticks(np.arange(0,201,50),[])
-
+    plt.legend(loc=4,bbox_to_anchor=(1,-0.2) ,fontsize=9)
    # plt.legend(loc=2, bbox_to_anchor=(1.63,0.5),borderaxespad = 0.)
 #    plt.suptitle('Neutron-proton scattering phase shifts (1).')
 
@@ -182,5 +229,6 @@ point_size         = 50
 x_fontsize         = 8
 y_fontsize         = 8
 kind               = 'cubic' 
-input_file_path = "./residual_data.txt"
-plot_phase_shift(input_file_path)
+input_file_path1 = "./residual_data_450.txt"
+input_file_path2 = "./residual_data_394.txt"
+plot_phase_shift(input_file_path1,input_file_path2)
