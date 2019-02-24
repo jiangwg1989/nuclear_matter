8574403d69ee2b3a9383f7c868dbccaa197a70f6
jiangwg1989 <jiangwg1989@gmail.com>
2019-02-24 17:52:04 -0500

    modified:   NNLO450/set20_Nmax14/Ca_chain_Rch.txt 	modified:   NNLO450/set20_Nmax14/info.txt 	modified:   charge_radii_calculator.py

--- plot_method/charge_radii_calculator.py
+++ plot_method/charge_radii_calculator.py
@@ -2,12 +2,13 @@ import numpy as np
 
 def charge_radii_calculator(r_point_square,Z,N):
     R_n_square = -0.1149
-    R_p_square = 0.8775*0.8775
-    r_charge_square =  r_point_square + R_p_square + N/Z*R_n_square + 0.033
+    R_p_square = 0.77000625
+    r_charge_square =  r_point_square + R_p_square + N*1.0/Z*R_n_square + 0.033
+    print(N*1.0/Z)
     return np.power(r_charge_square,0.5)
 
 
-r_c = charge_radii_calculator( 1.457311444*1.457311444  ,2,2)
+r_c = charge_radii_calculator( 12.08723450,20,34)
 
 print ('r_c='+str(r_c))
 
