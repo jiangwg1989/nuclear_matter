c6ae42d37bd5e351edaeb69a57439fc75a81fca5
w01 <jiangw@ornl.gov>
2019-03-29 17:30:25 -0400

    new file:   N_132.txt 	new file:   N_28.txt 	new file:   info.txt 	new file:   n_28vsn_132.pdf 	new file:   plot_N_28_vs_N_132.py 	modified:   ../../NNLO450/set20_Nmax14/plot_test.py 	modified:   ../../charge_radii_calculator.py

--- plot_method/NNLO450/set20_Nmax14/plot_test.py
+++ plot_method/NNLO450/set20_Nmax14/plot_test.py
@@ -5,10 +5,21 @@ import matplotlib.pyplot as plt
 
 
 fig = plt.figure()
-plt.subplot(121)
-rect = plt.Rectangle((0.1,0.1), 0.5, 0.3, fill=False, edgecolor = 'red',linewidth=1)
-ax = plt.gca()
-ax.add_patch(rect)
+#plt.subplot(121)
+#rect = plt.Rectangle((0.1,0.1), 0.5, 0.3, fill=False, edgecolor = 'red',linewidth=1)
+#ax = plt.gca()
+#ax.add_patch(rect)
+x_list= np.linspace(65,75,1000)
+y_list= np.zeros(1000)
+for loop  in range(0,1000):
+    y_list[loop] = 0.001079*pow(x_list[loop],4)-0.2974*pow(x_list[loop],3)+30.7*pow(x_list[loop],2)-1406*x_list[loop]+24130
+
+l = plt.plot(x_list,y_list)
+
+
+print(x_list)
+
+
 plot_path = 'test.eps'
 plt.savefig(plot_path)
 plt.show()
